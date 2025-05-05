ca7be9e61729fa9fbeb3fe0033747ad30d060b6b
Neonchikk <andreev_nn1577@mail.ru>
2025-05-05 12:00:12 +0300

    up tests

--- src/test/java/logic/db_tests/DBManagerTest.java
+++ src/test/java/logic/db_tests/DBManagerTest.java
@@ -41,6 +41,37 @@ class DBManagerTest {
     }
 
     @Test
+    @DisplayName("Проверка создания таблиц")
+    void testTablesCreation() throws Exception {
+        try (Connection connection = DBManager.getConnection();
+             Statement stmt = connection.createStatement()) {
+            assertDoesNotThrow(() -> stmt.executeQuery("SELECT 1 FROM speaker LIMIT 1"),
+                    "Таблица speaker должна существовать");
+
+            assertDoesNotThrow(() -> stmt.executeQuery("SELECT 1 FROM tag LIMIT 1"),
+                    "Таблица tag должна существовать");
+
+            assertDoesNotThrow(() -> stmt.executeQuery("SELECT 1 FROM meeting LIMIT 1"),
+                    "Таблица meeting должна существовать");
+
+            assertDoesNotThrow(() -> stmt.executeQuery("SELECT 1 FROM transcript LIMIT 1"),
+                    "Таблица transcript должна существовать");
+
+            assertDoesNotThrow(() -> stmt.executeQuery("SELECT 1 FROM replica LIMIT 1"),
+                    "Таблица replica должна существовать");
+
+            assertDoesNotThrow(() -> stmt.executeQuery("SELECT 1 FROM protocol LIMIT 1"),
+                    "Таблица protocol должна существовать");
+
+            assertDoesNotThrow(() -> stmt.executeQuery("SELECT 1 FROM task LIMIT 1"),
+                    "Таблица task должна существовать");
+
+            assertDoesNotThrow(() -> stmt.executeQuery("SELECT 1 FROM speaker_tag LIMIT 1"),
+                    "Таблица speaker_tag должна существовать");
+        }
+    }
+
+    @Test
     @DisplayName("Дефолтное добавление спикера")
     void testDefaultSpeakersAdded() {
         SpeakerDao speakerDao = DBManager.getSpeakerDao();
